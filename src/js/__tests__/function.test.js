6e2c531a1bf3543def538f45f1b38616a8dd556a
YuriyGl86 <yuriygl86@gmail.com>
2023-06-11 12:07:32 +0300

    first commit

---
+++ src/js/__tests__/function.test.js
@@ -0,0 +1,14 @@
+import showHealthColor from '../app.js'
+
+test.each([
+    ['health', 51, 'green'],
+    ['wounded', 50, 'yellow'],
+    ['critical', 1, 'red'],
+    ['dead', 0, undefined],
+    ])(
+    ('should return color for %s condition with %i level'),
+    (_, level, expectedColor) => {
+    const result = showHealthColor({health: level});
+    expect(result).toBe(expectedColor);
+});
+    
---
+++ src/js/app.js
@@ -0,0 +1,32 @@
+// TODO: write your code here
+
+
+const healthLevel = {
+    health: {
+        color: 'green',
+        level: 50
+    },
+    wounded : {
+        color: 'yellow',
+        level: 15
+    },
+    critical: {
+        color: 'red',
+        level: 0
+    }
+}
+
+export default function showHealthColor (character){
+    if(character.health > healthLevel.health.level) {
+        return healthLevel.health.color
+    }
+
+    if(character.health > healthLevel.wounded.level) {
+        return healthLevel.wounded.color
+    }
+
+    if(character.health > healthLevel.critical.level) {
+        return healthLevel.critical.color
+    }
+
+}
