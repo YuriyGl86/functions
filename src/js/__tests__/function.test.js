dba5d2a6632c81d5ff6860af40ecf2590c8a6dd2
YuriyGl86 <yuriygl86@gmail.com>
2023-06-11 12:40:40 +0300

    eslint_fix

--- src/js/__tests__/function.test.js
+++ src/js/__tests__/function.test.js
@@ -1,14 +1,14 @@
-import showHealthColor from '../app.js'
+import showHealthColor from '../app';
 
 test.each([
-    ['health', 51, 'green'],
-    ['wounded', 50, 'yellow'],
-    ['critical', 1, 'red'],
-    ['dead', 0, undefined],
-    ])(
-    ('should return color for %s condition with %i level'),
-    (_, level, expectedColor) => {
-    const result = showHealthColor({health: level});
+  ['health', 51, 'green'],
+  ['wounded', 50, 'yellow'],
+  ['critical', 1, 'red'],
+  ['dead', 0, undefined],
+])(
+  ('should return color for %s condition with %i level'),
+  (_, level, expectedColor) => {
+    const result = showHealthColor({ health: level });
     expect(result).toBe(expectedColor);
-});
-    
+  },
+);
